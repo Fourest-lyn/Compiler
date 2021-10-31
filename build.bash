bcad4108c6ed8f9d1c2330500df4662139d8f00d
Fourest <73587110+Fourest-lyn@users.noreply.github.com>
2021-10-31 23:58:40 +0800

    Add files via upload

--- build.bash
+++ build.bash
@@ -1,3 +1,3 @@
 set ff=UNIX
 mkdir runContain
-find ./src -name "*.java" -print0|xargs -0 javac -d runContain -cp ./antlr-4.9.2-complete.jar
+find ./src -name "*.java" -print0|xargs -0 javac -d runContain -cp ./antlr-4.9.1-complete.jar
