fcec5f12094c7ed7c7bb9f37e7853eca6feac977
Fourest <73587110+Fourest-lyn@users.noreply.github.com>
2021-10-31 23:57:12 +0800

    Add files via upload

---
+++ build.bash
@@ -0,0 +1,3 @@
+set ff=UNIX
+mkdir runContain
+find ./src -name "*.java" -print0|xargs -0 javac -d runContain -cp ./antlr-4.9.2-complete.jar
---
+++ semantic.bash
@@ -0,0 +1,3 @@
+set ff=UNIX
+set -e
+java -cp ./antlr-4.9.1-complete.jar:./runContain Main
